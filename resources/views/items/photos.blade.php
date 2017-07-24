78cb06f784441ff9f3f04bc3db078688e8e00ebc
Nick Baker <nick@webtechnick.com>
2017-07-24 00:29:32 -0600

    First image is larger

--- resources/views/items/photos.blade.php
+++ resources/views/items/photos.blade.php
@@ -1,12 +1,21 @@
 @foreach ($item->photos->chunk(3) as $set)
 <div class="row">
     @foreach ($set as $photo)
-        <div class="col-md-4 mb20 photo-bucket">
-            @include('photos._admindropdown')
-            <a href="/{{$photo->path}}" data-lightbox="item{{ $item->id }}" class="thumbnail">
-                <img class="img-rounded" src="{{ $photo->thumbnail(200) }}" alt="">
-            </a>
-        </div>
+        @if($loop->first)
+            <div class="col-md-12 mb20 photo-bucket">
+                @include('photos._admindropdown')
+                <a href="/{{$photo->path}}" data-lightbox="item{{ $item->id }}" class="thumbnail">
+                    <img class="img-rounded img-responsive" src="{{ $photo->thumbnail(500) }}" alt="">
+                </a>
+            </div>
+        @else
+            <div class="col-md-4 mb20 photo-bucket">
+                @include('photos._admindropdown')
+                <a href="/{{$photo->path}}" data-lightbox="item{{ $item->id }}" class="thumbnail">
+                    <img class="img-rounded img-responsive" src="{{ $photo->thumbnail(200) }}" alt="">
+                </a>
+            </div>
+        @endif
     @endforeach
 </div>
 @endforeach
