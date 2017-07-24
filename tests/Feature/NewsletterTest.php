852314823ad9871c47b06eb1f131da20ffac67d3
Nick Baker <nick@webtechnick.com>
2017-07-23 21:13:02 -0600

    Adding tests

--- tests/Feature/NewsletterTest.php
+++ tests/Feature/NewsletterTest.php
@@ -52,6 +52,24 @@ class NewsletterTest extends TestCase
     }
 
     /** @test */
+    public function user_can_subscribe_to_newsletter_via_post()
+    {
+        $this->mailgun = $this->mock(Mailgun::class, 'mailgun');
+        $this->mailgun->shouldReceive('post')
+                       ->with(
+                        'lists/newsletter@mandimakes.shop/members',
+                        [
+                            'address' => 'test@example.com',
+                            'subscribed' => 'yes',
+                            'upsert' => 'yes'
+                        ]
+                       )
+                       ->once();
+
+        $response = $this->post('/newsletters/subscribe', ['email' => 'test@example.com']);
+    }
+
+    /** @test */
     public function user_can_unsubscribe_from_newsletter()
     {
         $this->mailgun = $this->mock(Mailgun::class, 'mailgun');
@@ -60,5 +78,16 @@ class NewsletterTest extends TestCase
                       ->once();
 
         $response = $this->get('/newsletters/unsubscribe/test@example.com');
+    }
+
+    /** @test */
+    public function user_can_unsubscribe_from_newsletter_via_post()
+    {
+        $this->mailgun = $this->mock(Mailgun::class, 'mailgun');
+        $this->mailgun->shouldReceive('delete')
+                      ->with('lists/newsletter@mandimakes.shop/members/test@example.com')
+                      ->once();
+
+        $response = $this->post('/newsletters/unsubscribe', ['email' => 'test@example.com']);
     }
 }
