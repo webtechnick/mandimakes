9e6cd6c6438ae217e6e47b445a73cfae291fd2cb
Nick Baker <nick@webtechnick.com>
2017-07-24 23:00:07 -0600

    Adding posts table

---
+++ database/migrations/2017_07_24_204842_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('title');
+            $table->string('slug')->index();
+            $table->text('body');
+            $table->text('short_body');
+            $table->timestamp('published_at')->nullable()->index();
+            $table->tinyInteger('is_published')->unsigned()->index()->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
