e5a7eca7dd2fcb7ca381fdcc58ec407d46fba32d
= <=>
2018-03-05 02:05:04 +0000

    Adding after script

--- after.sh
+++ after.sh
@@ -3,3 +3,7 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+git config --global user.name = "Nick Baker"
+git config --global user.email = "nick@webtechnick.com"
+git config --global push.default simple
